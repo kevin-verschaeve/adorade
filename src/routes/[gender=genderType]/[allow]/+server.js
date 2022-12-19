347384831dc554d99582dcd8df62e9d06dc9689f
Kevin Verschaeve <kverschaeve@interdrinks.fr>
2022-12-19 19:07:31 +0100

    add route param matcher for gender

--- src/routes/[gender=genderType]/[allow]/+server.js
+++ src/routes/[gender=genderType]/[allow]/+server.js
@@ -4,11 +4,6 @@ import { db } from '$lib/firebase';
 
 export async function GET({ params }) {
   const allowed = params.allow === 'allow'; 
-
-  if (!['man', 'women'].includes(params.gender)) {
-    throw new redirect(301, '/');  
-  }
- 
   await addDoc(collection(db, 'adorade', params.gender, 'data'), {date: new Date(), allowed});
 
   throw redirect(301, '/');
